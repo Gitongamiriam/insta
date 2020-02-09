b0b5c6cdf2a4cecddea03e7538c4398c6cdda47d
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-09 19:41:43 +0300

    add a timezone in the model

--- instagram/models.py
+++ instagram/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-import datetime as dt
+from django.utils import timezone
 
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo=models.CharField(max_length=30,blank=True)
+    profile_photo=models.ImageField(upload_to='images/')
     bio=models.CharField(max_length=30,blank=True)
 
     def save_profile(self):
@@ -17,14 +17,13 @@ class Profile(models.Model):
         return self.profile_photo        
 
 class Image(models.Model):
-    image=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='images/')
     image_name=models.CharField(max_length=30)
     image_caption=models.CharField(max_length=30,blank=True)
     profile=models.ForeignKey(Profile)
     likes=models.IntegerField()
     comments=models.CharField(max_length=30,blank=True)
-    post_time=models.DateTimeField(auto_now_add=True)
-
+    timestamp = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.image_name
 
@@ -36,5 +35,5 @@ class Image(models.Model):
 
     @classmethod
     def show_images(cls):
-        return cls.objects.order_by('post_time')    
+        return cls.objects.order_by('timestamp')    
 
