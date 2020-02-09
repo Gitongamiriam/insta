d324dfa5430e79fcbc346c81cf7758b6294dbf83
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-09 21:55:08 +0300

    Add a class method for searched profiles

--- instagram/models.py
+++ instagram/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo=models.ImageField(upload_to='images/')
+    profile_photo=models.profileField(upload_to='profiles/')
     bio=models.CharField(max_length=30,blank=True)
 
     def save_profile(self):
@@ -16,25 +16,36 @@ class Profile(models.Model):
     def __str__(self):
         return self.bio        
 
-class Image(models.Model):
-    image=models.ImageField(upload_to='images/')
-    image_name=models.CharField(max_length=30)
-    image_caption=models.CharField(max_length=30,blank=True)
+class profile(models.Model):
+    profile=models.profileField(upload_to='profiles/')
+    profile_name=models.CharField(max_length=30)
+    profile_caption=models.CharField(max_length=30,blank=True)
     profile=models.ForeignKey(Profile)
     likes=models.IntegerField()
     comments=models.CharField(max_length=30,blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.image_name
+        return self.profile_name
 
-    def save_image(self):
+    def save_profile(self):
         self.save()
 
-    def delete_image(self):
+    def delete_profile(self):
         self.delete()
 
     @classmethod
-    def show_images(cls):
-        return cls.objects.order_by('timestamp')    
+    def get_profile_by_id(cls, id):
+        try:
+            profile = cls.objects.get(id=id)
+            print("Object found")
+            return profile
+        except DoesNotExist:
+            print("object not found")    
+
+    @classmethod
+    def search_profiles(cls, profile):
+        profile = profile.objects.get(name=profile)
+        profiles = cls.objects.filter(profile=profile.id)
+        return profiles)    
 
--- instagram/views.py
+++ instagram/views.py
@@ -7,3 +7,16 @@ def home(request):
     image=Image.objects.all()  
     profile=Profile.objects.all()
     return render(request,'home.html',{'image':image,'profile':profile})
+
+def search_results(request):
+
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Profile.search_by_profile(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'base.html/search_results.html',{"message":message,"profiles": searched_profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'base.tml/search_results.html',{"message":message,"profile":profile})    
