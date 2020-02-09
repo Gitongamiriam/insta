a5e0bff9672092a880d088e93f66197911048cac
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-09 19:00:52 +0300

    Add a class profile in the model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    profile_photo=models.CharField(max_length=30,blank=True)
+    bio=models.CharField(max_length=30,blank=True)
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        save.delete()
+
+    def __str__(self):
+        return self.profile_photo        
+
