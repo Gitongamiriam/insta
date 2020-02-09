1ce38d5601362ae7d8b390d8a385156518427027
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-10 00:13:34 +0300

    Add changes

--- instagram/views.py
+++ instagram/views.py
@@ -8,15 +8,15 @@ def home(request):
   
     return render(request,'home.html',{'posts':posts})
 
-# def search_results(request):
+def search_results(request):
 
-#     if 'profile' in request.GET and request.GET["profile"]:
-#         search_term = request.GET.get("profile")
-#         searched_profiles = Profile.search_by_profile(search_term)
-#         message = f"{search_term}"
+    if 'profile' in request.GET and request.GET["profile"]:
+        search_term = request.GET.get("profile")
+        searched_profiles = Image.search_by_profile('search_term')
+        message = f"{search_term}"
 
-#         return render(request, 'base.html/search_results.html',{"message":message,"profiles": searched_profiles})
+        return render(request, 'search_results.html',{"message":message,"profiles": searched_profiles})
 
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'base.tml/search_results.html',{"message":message,"profile":profile})    
+    else:
+        message = "You haven't searched for any profile"
+        return render(request, 'search_results.html',{"message":message})    
