b702f0cb2b82f6e60d86aedda56d0ec1b41d4bc6
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-09 17:22:36 +0300

    Add a template

--- instagram/views.py
+++ instagram/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('welcome to instagram')
+def home(request):
+    return render(request,'home.html')
