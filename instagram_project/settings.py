0bc28f8ba030ec7c657da995193f2d219c48cbd5
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-09 17:55:51 +0300

    Add a database

--- instagram_project/settings.py
+++ instagram_project/settings.py
@@ -77,11 +77,12 @@ WSGI_APPLICATION = 'instagram_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta2',
+        'USER': 'gitonga',
+    'PASSWORD':'gitonga254',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
